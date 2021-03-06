9dc394d2074cae37b459ccb8b93033c91845a41c
fabia moroni <fabia.moroni@gmail.com>
2021-03-06 20:19:06 +0100

    Updates

---
+++ precache-manifest.527a13d9a346a243776c6773b0c46380.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74debf06be391fbe1b44bd4210f004d0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ccd00a7f9bdd67b41e09",
+    "url": "/monsters-rolodex/static/css/main.01b6bfec.chunk.css"
+  },
+  {
+    "revision": "329197337dea8b5357b9",
+    "url": "/monsters-rolodex/static/js/2.62eef85e.chunk.js"
+  },
+  {
+    "revision": "ccd00a7f9bdd67b41e09",
+    "url": "/monsters-rolodex/static/js/main.cd0d5b03.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
